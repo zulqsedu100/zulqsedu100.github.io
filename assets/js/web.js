d1e8abb5150941dc09d0da7dea839ba0ccde7e41
Daffa Zulqisthi <107406360+zulqsedu100@users.noreply.github.com>
2022-06-14 18:38:23 +0700

    Add files via upload

---
+++ assets/js/web.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".btn").click(function() {
+        $(".submenu").slideToggle(1000);
+    });
+}); 
+
+function alrtBTN() {
+    alert ("Thanks for clicking!\nBut sorry this button is just for illustration.")
+};
